3a824b69034c850600c8ec31126b6bb2dc162f6b
fishxeno <wangweiyu94791358@gmail.com>
2026-02-10 15:31:09 +0800

    [deploy]

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,23 +1,13 @@
-import { createPool } from "mysql2";
+const mysql = require('mysql2');
 
-let pool;
-
-export default function initDB() {
-  pool = createPool({
+const db = mysql
+  .createPool({
     database: 'bumpbox',
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     timezone: "+00:00",
-    connectionLimit: 5
-  });
+}).promise()
 
-  pool.getConnection((err) => {
-    if (err) {
-      console.error("DB connection failed:", err);
-    } else {
-      console.log("DB connected");
-    }
-  });
-}
+module.exports = db;
