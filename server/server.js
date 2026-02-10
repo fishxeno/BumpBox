fa2417765fb16a1a028353e50fa8a316fea828df
fishxeno <wangweiyu94791358@gmail.com>
2026-02-10 15:06:15 +0800

    [deploy]

--- server/server.js
+++ server/server.js
@@ -2,31 +2,42 @@ import 'dotenv/config';
 import express, { raw, json, urlencoded, static as expressStatic } from 'express';
 import { resolve, join } from 'path';
 import methodOverride from 'method-override';
-
-// const stripePublishKey = process.env.STRIPE_PUBLISHABLE_KEY || undefined;
-// const stripeSecretKey = process.env.STRIPE_SECRET_KEY || undefined;
-// const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_KEY || undefined;
+import { createPool } from "mysql2";
 
 const app = express();
+const __dirname = resolve();
+//reverse proxy setup + static files
+app.use(expressStatic(join(__dirname, "public")));
+app.set('trust proxy', true);
+
+// Database connection
+const pool = createPool({
+	database: 'bumpbox',
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    timezone: "+00:00",
+    connectionLimit: 5
+  });
+
+  pool.getConnection((err) => {
+    if (err) {
+      	console.error("DB connection failed:", err);
+    } else {
+      	console.log("DB connected");
+    }
+  });
 
 app.post('/webhook', raw({ type: 'application/json' }), (req, res) => {
-  console.log(req.rawBody);
-  res.sendStatus(200);
+  	console.log(req.rawBody);
+  	res.sendStatus(200);
 });
 
 
 app.use(json()); // to parse json form data
 app.use(urlencoded({ extended: true }));
 app.use(methodOverride()); //override method names for older clients
-
-const __dirname = resolve();
-//reverse proxy setup + static files
-app.use(expressStatic(join(__dirname, "public")));
-app.set('trust proxy', true);
-
-import initDB from '../server/dbConnection.js';
-initDB();
-
 
 app.get('/api/items', (req, res) => {
   res.json({
@@ -40,5 +51,5 @@ app.get('/api/items', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  	console.log(`Server running on port ${PORT}`);
 });
